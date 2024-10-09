bfc0957fd3035ef38adac9dbf9ff29e9400a6cab
Daniel Stokes <danielstokes@newrelic.com>
2024-10-09 16:17:50 -0500

    fix: reorder import

--- src/apm/php_test.go
+++ src/apm/php_test.go
@@ -5,10 +5,11 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/newrelic/k8s-agents-operator/src/api/v1alpha2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+
+	"github.com/newrelic/k8s-agents-operator/src/api/v1alpha2"
 )
 
 func TestPhpInjector_Language(t *testing.T) {
