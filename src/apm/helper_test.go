e0a1040cb7673c9af3ff0e6039d683198a98c3a8
Ram Krishan Kumar <rkumar@newrelic.com>
2024-11-19 15:32:32 -0500

    push an easy test

--- src/apm/helper_test.go
+++ src/apm/helper_test.go
@@ -1,6 +1,13 @@
 package apm
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 func TestBaseInjector_ConfigureClient(t *testing.T) {
 
@@ -20,4 +27,47 @@ func TestInjectorRegistery_MustRegister(t *testing.T) {
 
 func TestInjectors_Names(t *testing.T) {
 
+}
+
+func TestApplyLabel(t *testing.T) {
+	tests := []struct {
+		name           string
+		pod            *corev1.Pod
+		expectedPod    *corev1.Pod
+		expectedErrStr string
+	}{
+		{
+			name: "a container with labels added",
+			pod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name: "test",
+						Env: []corev1.EnvVar{
+							{Name: "NEW_RELIC_LABELS", Value: "app:java-injected"},
+						}}}}},
+
+			expectedPod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"foo": "bar"},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name: "test",
+						Env: []corev1.EnvVar{
+							{Name: "NEW_RELIC_LABELS", Value: "app:java-injected"},
+						},
+					}},
+				}},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actualPod := ApplyLabel(test.pod, "foo", "bar")
+
+			if diff := cmp.Diff(test.expectedPod, actualPod); diff != "" {
+				assert.Fail(t, diff)
+			}
+		})
+	}
 }
