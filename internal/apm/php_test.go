c927660f4a82f9cb54d64bee325a873055270390
Daniel Stokes <danielstokes@newrelic.com>
2025-03-26 09:54:48 -0500

    add test case, reduce whats copied from container to init container

--- internal/apm/php_test.go
+++ internal/apm/php_test.go
@@ -98,8 +98,58 @@ func TestPhpInjector_Inject(t *testing.T) {
 					InitContainers: []corev1.Container{{
 						Name: "newrelic-instrumentation-php",
 						Env: []corev1.EnvVar{
+							{Name: "NEW_RELIC_APP_NAME", Value: "test"},
+							{Name: "NEW_RELIC_LABELS", Value: "operator:auto-injection"},
+							{Name: "NEW_RELIC_K8S_OPERATOR_ENABLED", Value: "true"},
+							{Name: "NEW_RELIC_LICENSE_KEY", ValueFrom: &corev1.EnvVarSource{SecretKeyRef: &corev1.SecretKeySelector{LocalObjectReference: corev1.LocalObjectReference{Name: "newrelic-key-secret"}, Key: "new_relic_license_key", Optional: &vtrue}}},
+						},
+						Command:      []string{"/bin/sh"},
+						Args:         []string{"-c", "cp -a /instrumentation/. /newrelic-instrumentation/ && /newrelic-instrumentation/k8s-php-install.sh 20230831 && /newrelic-instrumentation/nr_env_to_ini.sh"},
+						VolumeMounts: []corev1.VolumeMount{{Name: "newrelic-instrumentation", MountPath: "/newrelic-instrumentation"}},
+					}},
+					Volumes: []corev1.Volume{{Name: "newrelic-instrumentation", VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}}}},
+				},
+			},
+			inst: current.Instrumentation{Spec: current.InstrumentationSpec{Agent: current.Agent{Language: "php-8.3"}, LicenseKeySecret: "newrelic-key-secret"}},
+		},
+		{
+			name: "a container, instrumentation, with existing env PHP_INI_SCAN_DIR",
+			pod: corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{"instrumentation.newrelic.com/php-version": "8.3"}},
+				Spec: corev1.PodSpec{Containers: []corev1.Container{
+					{
+						Name: "test",
+						Env: []corev1.EnvVar{
 							{Name: "a", Value: "a"},
-							{Name: "PHP_INI_SCAN_DIR", Value: "/newrelic-instrumentation/php-agent/ini"},
+							{Name: "PHP_INI_SCAN_DIR", Value: "fakepath"},
+						},
+					},
+				}},
+			},
+			expectedPod: corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"instrumentation.newrelic.com/php-version": "8.3",
+						"newrelic.com/instrumentation-versions":    `{"/":"/0"}`,
+					},
+					Labels: map[string]string{
+						DescK8sAgentOperatorVersionLabelName: version.Get().Operator},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name: "test",
+						Env: []corev1.EnvVar{
+							{Name: "a", Value: "a"},
+							{Name: "PHP_INI_SCAN_DIR", Value: "fakepath:/newrelic-instrumentation/php-agent/ini"},
+							{Name: "NEW_RELIC_APP_NAME", Value: "test"},
+							{Name: "NEW_RELIC_LABELS", Value: "operator:auto-injection"},
+							{Name: "NEW_RELIC_K8S_OPERATOR_ENABLED", Value: "true"},
+						},
+						VolumeMounts: []corev1.VolumeMount{{Name: "newrelic-instrumentation", MountPath: "/newrelic-instrumentation"}},
+					}},
+					InitContainers: []corev1.Container{{
+						Name: "newrelic-instrumentation-php",
+						Env: []corev1.EnvVar{
 							{Name: "NEW_RELIC_APP_NAME", Value: "test"},
 							{Name: "NEW_RELIC_LABELS", Value: "operator:auto-injection"},
 							{Name: "NEW_RELIC_K8S_OPERATOR_ENABLED", Value: "true"},
